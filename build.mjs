1efd0303c315df6b9a69974522dae1e5e423b05b
EBIHARA Kenji <kenji.ebihara@jp.ricoh.com>
2019-05-16 09:34:01 +0900

    ビルド環境の見直し

--- build.mjs
+++ build.mjs
@@ -3,7 +3,7 @@ import fs from 'fs-extra';
 
 fs.emptyDirSync('./dist');
 const yarn = 'yarn' + (process.platform === 'win32' ? '.cmd' : '');
-const tsc = cp.spawn(yarn, ['tsc']);
+const tsc = cp.spawn(yarn, ['tsc', '-p', 'tsconfig.build.json']);
 tsc.stdout.on("data", (buf) => {
     console.log(buf.toString('utf-8', 0, buf.length));
 });
