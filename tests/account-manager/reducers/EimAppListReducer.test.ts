6d482acca648271ce04d017ab3fac4069649f199
EBIHARA Kenji <kenji.ebihara@jp.ricoh.com>
2019-05-24 13:17:29 +0900

    EIMAppListReducer の UT を完了した。

---
+++ tests/account-manager/reducers/EimAppListReducer.test.ts
@@ -0,0 +1,51 @@
+import { ISetAppListAction, LOAD_APP_LIST, SET_APP_LIST } from '../../../src/account-manager/actions/EimAppListActions';
+import appListReducer from '../../../src/account-manager/reducers/EimAppListReducer';
+import { IEimApp } from '../../../src/account-manager/states/IEimAppListState';
+
+describe('', () => {
+    test('init', () => {
+        const initState = {
+            appList: [],
+            loading: false,
+        };
+        const state = appListReducer(undefined, { type: '' });
+        expect(state).toEqual(initState);
+    });
+    test('SET_APP_LIST', () => {
+        const initState = {
+            appList: [],
+            loading: true,
+        };
+        const appList: IEimApp[] = [
+            {
+                appKey: 'app-key',
+                appName: 'app-name',
+                description: 'desc',
+                siteDomain: 'site-domain',
+                siteName: 'site-name',
+                tokens: ['token'],
+            },
+        ];
+        const action: ISetAppListAction = {
+            appList,
+            type: SET_APP_LIST,
+        };
+        const state = appListReducer(initState, action);
+        expect(state).toEqual({
+            appList,
+            loading: false,
+        });
+    });
+    test('LOAD_APP_LIST', () => {
+        const initStae = {
+            appList: [],
+            loading: false,
+        };
+        const action = { type: LOAD_APP_LIST };
+        const state = appListReducer(initStae, action);
+        expect(state).toEqual({
+            appList: [],
+            loading: true,
+        });
+    });
+});
