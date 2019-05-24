66361438a6f260a532f7738576933a11bdb9da4a
EBIHARA Kenji <kenji.ebihara@jp.ricoh.com>
2019-05-24 11:31:35 +0900

    AccountListReducer のUTが完了

---
+++ tests/account-manager/reducers/AccountListReducer.test.ts
@@ -0,0 +1,68 @@
+import {
+    ISetAuthState,
+    IShowAccountListAction,
+    SET_AUTH_STATE,
+    SHOW_ACCOUNT_LIST,
+} from '../../../src/account-manager/actions/AccountListActions';
+import accountListReducer from '../../../src/account-manager/reducers/AccountListReducer';
+import { IAccountState } from '../../../src/account-manager/states/IAccountState';
+import { IAuthState } from '../../../src/account-manager/states/IAuthStates';
+
+const initState = {
+    accounts: [],
+    authState: {},
+};
+describe('', () => {
+
+    test('init', () => {
+        const state = accountListReducer(undefined, { type: '' });
+        expect(state).toEqual({
+            accounts: [],
+            authState: {},
+        });
+    });
+    test('SHOW_ACCOUNT_LIST', () => {
+        const expectAccounts: IAccountState[] = [{
+            authType: 'password',
+            eimToken: ['token'],
+            id: 'a001',
+            lastConnect: new Date(0),
+            password: 'password',
+            siteDomain: 'domain',
+            siteName: 'site-name',
+            userId: 'user-id',
+        }];
+        const action: IShowAccountListAction = {
+            accountList: {
+                accounts: expectAccounts,
+                authState: { appKey: 'appkey' },
+            },
+            type: SHOW_ACCOUNT_LIST,
+        };
+        const state = accountListReducer(initState, action);
+        expect(state).toEqual({
+            accounts: expectAccounts,
+            authState: {},
+        });
+    });
+    test('SET_AUTH_STATE', () => {
+        const authState: IAuthState = {
+            appKey: 'app-key',
+            appKeyPrefix: 'app-key-prefix',
+            link: 'link',
+            mobileAppKey: 'mobile-app-key',
+            siteDomain: 'site-domain',
+            siteName: 'site-name',
+            tokens: ['token'],
+        };
+        const action: ISetAuthState = {
+            authState: authState,
+            type: SET_AUTH_STATE,
+        };
+        const state = accountListReducer(initState, action);
+        expect(state).toEqual({
+            accounts: [],
+            authState,
+        });
+    });
+});
